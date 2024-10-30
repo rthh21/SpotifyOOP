3fdab0d179edf6536c700d229feef28c9b90f93a
Andrew <andreythstan25@gmail.com>
2024-10-30 17:03:00 +0200

    Clase adaugate: Artist, Album, Song

--- main.cpp
+++ main.cpp
@@ -1,57 +1,59 @@
 #include <iostream>
 #include <array>
-
 #include <Helper.h>
 
+class Song{
+    public:
+        std::string title;
+        std::string genre;
+        int duration;
+        
+        Song(const std::string title, const std::string genre, const int duration){
+            this->title = title;
+            this->genre = genre;
+            this->duration = duration;
+        }
+};
+
+class Album{
+    public:
+        std::string name;
+        std::string genre;
+        std::vector<Song> songs;
+        
+        Album(const std::string name, const std::string genre){
+            this->name = name;
+            this->genre = genre;
+        }
+        
+        void songAdd(const Song &song){
+            songs.push_back(song);
+        }      
+};
+
+class Artist{
+    public:
+        std::string name;
+        std::string genre;
+        std::vector<Album> albums;
+        
+        Artist(const std::string name, const std::string genre){
+            this->name = name;
+            this->genre = genre;
+        }
+        
+        void albumAdd(const Album& album){
+            albums.push_back(album);
+        }
+};
+
+
 int main() {
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
-    }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
-    ///                Exemplu de utilizare cod generat                     ///
-    ///////////////////////////////////////////////////////////////////////////
+    
+    Artist artist1("Travis Scott", "RAP");
+    
     Helper helper;
     helper.help();
-    ///////////////////////////////////////////////////////////////////////////
+    
     return 0;
 }
