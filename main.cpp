c48ad6037067a9c0a08bd0f5d7748240491b31e9
Andrew <andreythstan25@gmail.com>
2024-10-30 20:31:45 +0200

    Completari clase, adaugare metode pentru artisti, albume, melodii si playlisturi noi. Plus mod consola cu functie de iesire.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <array>
 #include <Helper.h>
 #include <vector>
+#include <cstring>
 
 class Song{
     public:
@@ -9,10 +10,21 @@ class Song{
         std::string genre;
         int duration;
         
+        Song(){
+            this->title = "null";
+            this->genre = "null";
+            this->duration = 0;
+        }
+        
         Song(const std::string title, const std::string genre, const int duration){
             this->title = title;
             this->genre = genre;
             this->duration = duration;
+        }
+        
+        friend std::ostream& operator<<(std::ostream& os, const Song& song){
+            os << "Song name: "<<song.title<<", genre: "<<song.genre<<", and duration: "<<song.duration;
+            return os;
         }
 };
 
@@ -27,9 +39,14 @@ class Album{
             this->genre = genre;
         }
         
-        void songAdd(const Song &song){
+        void addSongInAlbum(const Song &song){
             songs.push_back(song);
-        }      
+        }   
+        
+        friend std::ostream& operator<<(std::ostream& os, const Album& album) {
+        os << "Album Name: " << album.name << ", Genre: " << album.genre;
+        return os;
+    }
 };
 
 class Artist{
@@ -37,23 +54,109 @@ class Artist{
         std::string name;
         std::string genre;
         std::vector<Album> albums;
+        std::vector<Song> songs;
         
         Artist(const std::string name, const std::string genre){
             this->name = name;
             this->genre = genre;
         }
         
-        void albumAdd(const Album& album){
+        void addAlbum(const Album& album){
             albums.push_back(album);
+        }
+        
+        void addSongToArtist(const Song& song){
+            songs.push_back(song);
+        }
+        
+        friend std::ostream& operator<<(std::ostream& os, const Artist& artist){
+            os << "Artist Name: " << artist.name << ", Genre: " << artist.genre;
+            return os;
         }
 };
 
+class Playlist{
+    private:
+        std::vector<Song> songs;
+    public:
+        std::string name;
+        std::string createdBy;
+    
+    //constructor
+    Playlist(){
+        this->name = "null";
+        this->createdBy = "null";
+    }
+    
+    Playlist(const std::string name, const std::string createdBy){
+        this->name = name;
+        this->createdBy = createdBy;
+    }
+    
+    //contructor copy
+    Playlist(const Playlist& CpyPlaylist){
+        this->name = CpyPlaylist.name;
+        this->createdBy = CpyPlaylist.createdBy;
+        this->songs = CpyPlaylist.songs;
+        std::cout<<"Copy constructor for playlist: "<<this->name<<std::endl;
+    }
+    
+    //destructor
+    ~Playlist(){
+        std::cout<<"Destructor for playlist.\n";
+    }
+    
+    //Operator '='
+    Playlist& operator=(const Playlist& fPlaylist){
+        this->name = fPlaylist.name;
+        this->createdBy = fPlaylist.createdBy;
+        this->songs = fPlaylist.songs;
+        std::cout<<" Succesfully '=' !\n";
+        return *this;
+    }
+    
+    void addSongInPlaylist(const Song& song){
+        songs.push_back(song);
+    }
+    void removeSongPosInPlaylist(int x){
+        songs.erase(songs.begin()+x);
+    }
+    
+    friend std::ostream& operator<<(std::ostream& os, const Playlist& playlist){
+        os << "Playlist Name: " << playlist.name << ", Genre: " << playlist.createdBy;
+        return os;
+    }
+};
 
 int main() {
+    std::cout<<"Type 'exit' to end the program.";
+    std::string token;
     
-    Artist artist1("Travis Scott", "RAP");
-    std::cout<<artist1.name;
-
+    while (1) {
+        std::cout<<"\n> ";
+        std::getline(std::cin, token);
+        
+        if (token.find("exit") != std::string::npos) 
+            break;
+        
+        if(token.find("new") != std::string::npos){
+            //TODO
+            if(token.find("song") != std::string::npos){
+                std::string title, genre;
+                int duration;
+                std::cout<<"\nEnter song title: ";
+                std::getline(std::cin, title);
+                std::cout<<"Enter song genre: ";
+                std::getline(std::cin, genre);
+                std::cout<<"Enter song duration: ";
+                std::cin>>duration;
+                Song song1(title,genre,duration);
+                std::cout<<song1;
+            }
+            //TODO
+        }
+    }
+    
     Helper helper;
     helper.help();
     
